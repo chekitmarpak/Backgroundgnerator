a3df969b845a587ea4ed5855b2d296b91c3bae1c
chekitmarpak <chekitmeher@gmail.com>
2021-06-13 06:21:45 +0530

    fourth commit

--- sam.js
+++
@@ -1,8 +0,0 @@
-import { createServer, Server } from 'http';
-
-createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(`Hello World!`);
-});
-
-Server.listen(3000);
