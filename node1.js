5ecdb7983e07363d4a47df6fbf7ed707fbc3d4e0
chekitmarpak <chekitmeher@gmail.com>
2021-06-15 13:23:26 +0530

    twelvth commit

--- node1.js
+++ node1.js
@@ -6,7 +6,7 @@ async function main() {
     const client = new MongoClient(uri);
     try{
         await client.connect();
-        await listDatabases(client);
+        await findOneListingByName(client, "Veg Junction");
     } catch (e) {
         console.error(e);
     }
@@ -17,6 +17,33 @@ async function main() {
 }
 
 main().catch(console.error);
+
+async function findOneListingByName(client, nameOfListing) {
+    const result = await client.db("sample_restaurants").collection("restaurants").findOne({name: nameOfListing});
+
+    if (result) {
+        console.log(`found a listing in the collection with the name '${nameOfListing}'`);
+        console.log(result);
+    } else {
+        console.log(`No listings found with the name '${nameOfListing}'`);
+    }
+}
+
+async function createMultipleListings(client, newListings) {
+
+    const result = await client.db("sample_restaurants").collection("restaurants").insertMany(newListings);
+
+    console.log(`${result.insertedCount} new listings created with the following Id(s):`);
+    console.log(result.insertedIds);
+
+}
+
+async function createListing(client, newListing) {
+    
+    const result = await client.db("sample_restaurants").collection("restaurants").insertOne(newListing);
+
+    console.log(`New Listing created with the following id: ${result.insertedId}`);
+}
 
 async function listDatabases(client){
     const databasesList = await client.db().admin().listDatabases();
